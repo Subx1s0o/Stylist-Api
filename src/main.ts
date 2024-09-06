8ceac25c56a31bc3b82c6aaba83f3a60704fcd49
Subx1s0o <nazarsabardin@gmail.com>
2024-09-06 22:39:21 +0300

    FIX: update cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://stylist-marina.vercel.app'],
+  });
   const port = process.env.PORT || 8080;
+
   await app.listen(port);
 }
 bootstrap();
